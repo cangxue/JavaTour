c3939968728013247eea8353cc84738f96ea4529
cangxue <752787086@qq.com>
2017-04-24 15:35:00 +0800

    build HelloWorld Class

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Created by xiaoma on 17/4/24.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        //输出Hello，World！
+        System.out.println("Hello, World!");
+    }
+}
