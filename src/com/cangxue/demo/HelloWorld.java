d3dcc65e93d01cbd6f19698dfa67cc20e6fcf6de
xiaoma <752787086@qq.com>
2019-06-03 18:04:17 +0800

    welcome

---
+++ src/com/cangxue/demo/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.cangxue.demo;
+
+/**
+ * Created by cangxue on 19/6/3.
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello,World!");
+    }
+}
